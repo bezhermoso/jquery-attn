600e9a143387007ed7a6cc32b5d6358e5c921f31
Bez Hermoso <bezalelhermoso@gmail.com>
2013-07-17 10:33:35 +0800

    Updated the usage examples.

--- assets/js/jquery.attn.usage.js
+++ assets/js/jquery.attn.usage.js
@@ -3,14 +3,15 @@ jQuery(function($){
     
     $('body').attn({});
     
+    $('body').attn('success', 'Success message');
     $('body').attn('error', 'Error message - Will fade out in 3 seconds', 3000);
-    $('body').attn('success', {
-        message: 'Success message'
-    });
     $('body').attn('warning', 'Warning message - Will fade out in 5 seconds.', {
         fade: 5000
     });
-    $('body').attn('info', 'Info message - Will fade in 7 seconds', 7000);
+    $('body').attn('info', {
+        message: 'Info message - will fade out in 7 seconds',
+        fade: 7000
+    });
     
 });
 

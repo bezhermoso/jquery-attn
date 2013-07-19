24a7149fb2b319c091e9e0361120e2bf538188b4
Alel Hermoso <bezalelhermoso@gmail.com>
2013-07-19 13:45:24 +0800

    Wrote Github page for the project. Removed the usage examples.

--- assets/js/jquery.attn.usage.js
+++ assets/js/jquery.attn.usage.js
@@ -5,39 +5,41 @@ jQuery(function($){
         container: '#attn-container'
     });
     
-    $('body').attn('success', 'Success message');
-    $('body').attn('error', 'Error message - Will fade out in 3 seconds', 3000);
-    $('body').attn('warning', 'Warning message - Will fade out in 5 seconds.', {
-        fade: 5000
-    });
-    $('body').attn('info', {
-        message: 'Info message - will fade out in 7 seconds',
-        fade: 7000
-    });
-    
-    $('body').attn('warning', {
-        message: 'See what happens when you close me.',
-        onBeforeClose: function(event){
-            var prompt = confirm('Tada! This is kind of lame. But you can actually do whatever you want here!\n\nPress OK to continue removal of attention item. Click Cancel to skip.');
-            if(!prompt){
-                event.preventDefault();
-            }
-        },
-        onAfterClose: function(){
-            $('body').attn('clear');
-            $('body').attn('success', '<strong><em>Alors, au revoir!</em></strong>');
-        }
-    });
-    
-    var forgotPassItem = $('body').attn('error', {
-        target: '#forgot-password-feedback',
-        classes: 'alert-error alert-block'
-    });
-    
-    $('#forgot-password-feedback button').click(function(){
-        forgotPassItem.dismiss();
-        $('body').attn('success', 'A password reset link was sent to your email address.');
-    });
+    $('body').attn('success', '<strong>Welcome, stranger!</strong>');
+//    
+//    $('body').attn('success', 'Success message');
+//    $('body').attn('error', 'Error message - Will fade out in 3 seconds', 3000);
+//    $('body').attn('warning', 'Warning message - Will fade out in 5 seconds.', {
+//        fade: 5000
+//    });
+//    $('body').attn('info', {
+//        message: 'Info message - will fade out in 7 seconds',
+//        fade: 7000
+//    });
+//    
+//    $('body').attn('warning', {
+//        message: 'See what happens when you close me.',
+//        onBeforeClose: function(event){
+//            var prompt = confirm('Tada! This is kind of lame. But you can actually do whatever you want here!\n\nPress OK to continue removal of attention item. Click Cancel to skip.');
+//            if(!prompt){
+//                event.preventDefault();
+//            }
+//        },
+//        onAfterClose: function(){
+//            $('body').attn('clear');
+//            $('body').attn('success', '<strong><em>Alors, au revoir!</em></strong>');
+//        }
+//    });
+//    
+//    var forgotPassItem = $('body').attn('error', {
+//        target: '#forgot-password-feedback',
+//        classes: 'alert-error alert-block'
+//    });
+//    
+//    $('#forgot-password-feedback button').click(function(){
+//        forgotPassItem.dismiss();
+//        $('body').attn('success', 'A password reset link was sent to your email address.');
+//    });
     
 });
 
--- index.php
+++ index.php
@@ -7,18 +7,36 @@
         <script src="//google-code-prettify.googlecode.com/svn/loader/run_prettify.js"></script>
         <script src="assets/js/jquery-attn/jquery.attn.js"></script>
         <script src="assets/js/jquery.attn.usage.js"></script>
+        <link rel="stylesheet" href="assets/css/sons-of-obsidian.css" type="text/css" />
         <link rel="stylesheet" href="assets/css/bootstrap.css" type="text/css" />
-        <link rel="stylesheet" href="//google-code-prettify.googlecode.com/svn/trunk/styles/sons-of-obsidian.css" type="text/css" />
+        <link rel="stylesheet" href="assets/css/docs.css" type="text/css" />
         <style>
-            body
-            {
-                padding-top: 20px;
-            }
         </style>
     </head>
     <body>
+        <div class="lead-block">
+            <div class="container hero-unit">
+                <h1>jquery-attn.js</h1>
+                <p class="lead">
+                    jQuery Attention -- a feedback-message/notification plugin for jQuery.
+                </p>
+            </div>
+        </div>
         <div class="container">
             <ul id="attn-container" class="unstyled"></ul>
+            <p>
+                The notification message you see above is produced by the <code>jquery-attn.js</code> plugin!
+                <pre class="prettyprint lang-js linenums">
+     
+    //Initialize a notification area for the 'body'. Messages will appear within &lt;ul id="attn-container"&gt;...&lt;/ul&gt;
+    $('body').attn({
+        container: '#attn-container'
+    });
+
+    //Add a success message to the notification area attached to 'body'.
+    $('body').attn('success', '&lt;strong&gt;Welcome, stranger!&lt;/strong&gt;');
+                </pre>
+            </p>
         </div>
         <div class="hide">
             <span id="forgot-password-feedback">

e014c44e27255fbdcdc9130509f7828d2f664caf
Alel Hermoso <bezalelhermoso@gmail.com>
2013-07-19 11:01:34 +0800

    Reorganized jquery-attn assets.

--- index.php
+++ index.php
@@ -4,9 +4,8 @@
         <meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
         <title>jQuery Attention</title>
         <script src="//ajax.googleapis.com/ajax/libs/jquery/1.10.2/jquery.min.js"></script>
-        <script src="assets/js/jquery.attn.js"></script>
+        <script src="assets/js/jquery-attn/jquery.attn.js"></script>
         <script src="assets/js/jquery.attn.usage.js"></script>
-        <link rel="stylesheet" href="assets/css/attn.css" type="text/css" />
         <link rel="stylesheet" href="assets/css/bootstrap.css" type="text/css" />
         <style>
             body
